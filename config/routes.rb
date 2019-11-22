d11e5052a7c70decf7bdc584b6548c0c4ab6f99f
liting zhao <emily51905@163.com>
2019-11-22 13:13:38 +0800

    set a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails do
+  root to: 'cocktails#index'
+  resources :doses, only: :destroy
+  resources :cocktails, except: :index do
     resources :doses, only: [:new, :create, :index]
   end
-  resources :doses, only: [:destroy]
 end
